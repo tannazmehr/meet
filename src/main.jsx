aa1d3969f1d472c86b705fb24039f37251b6a54a
tannazmehr <tannazmehrjooee@gmail.com>
2025-02-23 16:35:59 +0100

    added manifest and service woker

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
+//import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-atatus.config('13c43af75dc449ddb6312cbb425f06ca').install();
+//atatus.config('13c43af75dc449ddb6312cbb425f06ca').install();
 
 
 
@@ -12,4 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App /> 
   </React.StrictMode>,
-)
+);
+
+serviceWorkerRegistration.register();
